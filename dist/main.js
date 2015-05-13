282fe708ceb4c27c5d8503192c4253c7efa65225
Keith Reynolds <keithreynoldsworld@gmail.com>
2015-05-13 14:54:51 -0500

    damnloop

---
+++ dist/main.js
@@ -0,0 +1,47 @@
+function* guessingGame() {
+	/*console.log(yield game.say('Hey there!'));
+	console.log(yield game.say('Woah', 'This is cool'));
+	console.log(yield game.ask('Woah', 'This is cool'));
+	console.log(yield game.choose('Woah', 'This is cool', 'Hello world'));
+	console.log(yield game.say('That\'s all folks!', 'THE END')); */
+
+	var x = Math.floor(Math.random() * 100) + 1;
+	var Correct = false;
+	var theirGuess = yield game.ask
+	('I be thinky of a number from 1 to 100. What do you think my secret number is?');
+   
+while(true){ 
+
+	if (theirGuess < x){yield game.say('too low. try again');
+		theirGuess = yield game.ask
+	('I be thinky of a number from 1 to 100. What do you think my secret number is?');
+
+
+
+	}
+    
+
+	else if (theirGuess > x){yield game.say('like Doug Benson, too high');
+	theirGuess = yield game.ask
+	('I be thinky of a number from 1 to 100. What do you think my secret number is?');
+
+	 
+	}
+
+
+	else if (theirGuess === x){yield game.say('YOU GoT IT RIGHT');
+							
+
+
+    
+
+} ;
+
+
+
+}
+
+}
+
+	
+	
